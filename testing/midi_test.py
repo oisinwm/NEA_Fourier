a310bff3f48423c7f599f7d0b4d96358c4287d9b
Oisin Wellesley-Miller <oisinwm@gmail.com>
2019-03-10 21:01:21 +0000

    Added Variable Length Values

--- testing/midi_test.py
+++ testing/midi_test.py
@@ -20,5 +20,20 @@ class Midi:
         pass
     
     def vlq(self, value):
-        pass
-
+        bits = list(bin(value)[2:])
+        while len(bits) % 7 != 0:
+            bits = ["0"] + bits
+        rev_bits = bits[::-1]
+        result = []
+        for i, value in enumerate(rev_bits):
+            result.append(value)
+            if (i+1) == 7:
+                result.append("0")
+            elif (i+1) % 7 == 0:
+                result.append("1")
+        binary_str = "".join(result)[::-1]
+        hex_result = [hex(int(binary_str[i:i + 4],2))[2:] for i in range(0, len(binary_str), 4)]
+        return "".join(hex_result)
+        
+if __name__ == "__main__":
+    a = Midi()
